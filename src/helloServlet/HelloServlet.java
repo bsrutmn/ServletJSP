b42a65929d02275705304bd122fadf47bb61e64c
Busra Utman <busrautmn@gmail.com>
2018-12-13 18:33:24 +0300

    created first page

---
+++ src/helloServlet/HelloServlet.java
@@ -0,0 +1,19 @@
+package helloServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet{
+	public void doGet(HttpServletRequest request , HttpServletResponse response) throws IOException {
+		PrintWriter	printWriter = response.getWriter();
+		printWriter.write("<html><body>");
+		String message="<h1>Hello Servlet! My name is busra :) </h1>";
+		printWriter.write(message);
+		printWriter.write("</body></html>");
+	}
+
+}
---
+++ src/helloServlet/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author busra , busrautmn
+ * , WithLove <3 17:26:50 13 Ara 2018
+ */
+package helloServlet;
