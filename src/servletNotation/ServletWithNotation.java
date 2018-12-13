4cc02876596d7d7c26eac256400de1ae8d7c3b90
Busra Utman <busrautmn@gmail.com>
2018-12-13 19:57:46 +0300

    used @WebServlet(url pattern)

--- src/servletNotation/ServletWithNotation.java
+++ src/servletNotation/ServletWithNotation.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/helloServlet")
+@WebServlet(urlPatterns = {"/merhabaServlet","/helloServlet"})
 public class ServletWithNotation extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
