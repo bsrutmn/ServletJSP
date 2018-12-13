dea1fd40879242f9b4f7f4addaac4281afb1ee0d
Busra Utman <busrautmn@gmail.com>
2018-12-13 19:46:15 +0300

    added webServlet annotation usage

--- src/helloServlet/HelloServlet.java
+++ src/helloServlet/HelloServlet.java
@@ -11,7 +11,7 @@ public class HelloServlet extends HttpServlet{
 	public void doGet(HttpServletRequest request , HttpServletResponse response) throws IOException {
 		PrintWriter	printWriter = response.getWriter();
 		printWriter.write("<html><body>");
-		String message="<h1>Hello Servlet! My name is busra :) </h1>";
+		String message="<h1>Hello Servlet! My name is busra :) Welcome </h1>";
 		printWriter.write(message);
 		printWriter.write("</body></html>");
 	}
